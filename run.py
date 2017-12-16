68d741385d2742a35dad4233c8181c31d00d22d1
kmiller96 <kale.miller96@gmail.com>
2017-12-16 09:43:43 +0800

    Initialised executable file

---
+++ run.py
@@ -0,0 +1,17 @@
+# AUTHOR: Kale Miller
+# DESCRIPTION: Collects the weather data in WA.
+
+import urllib2
+from bs4 import BeautifulSoup
+import os, zipfile, glob, pandas
+
+
+def main():
+    """Runs the main script."""
+    return
+
+
+if __name__ == '__main__':
+    main()
+
+print "Executed successfully."
