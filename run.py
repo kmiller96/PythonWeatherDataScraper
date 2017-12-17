6c34edff4db86a2c4d535d6ed0a11dd1fb2f1c42
kmiller96 <kale.miller96@gmail.com>
2017-12-17 18:21:29 +0800

    Refactored the script to account for other data types
    The old script worked well, but was hardly extendable. If I wanted to
    include processes to fetch temperature and solar exposure I would end
    up repeating myself (remember DRY). As such, I restructured my code so
    that it would be easily extendable.
    
    Now each data "type" will inherit from a parent class and define their
    own methods for each specific case.
    
    This also gives the unintended benefit of being able to store
    information about each station like it's coordinates (which will be
    useful in the data visualisation).

--- run.py
+++ run.py
@@ -3,106 +3,128 @@
 
 from bs4 import BeautifulSoup
 import pandas as pd
-import urllib2, os, zipfile, glob
+import urllib2, os, zipfile, glob, shutil
 
+###########################
+# DEFINE GLOBAL FUNCTIONS #
+###########################
 
 def fetchStationList(station_csv_name):
     """Extracts the station list from the csv file."""
     station_df = pd.read_csv(station_csv_name)
     return station_df['Site']
 
-
-def downloadDataForStation(n, datadir='./', verbose=False, skipexistingdata=False):
-    """Fetches the raw zipped file from BOM to process."""
-
-    # If the data already exists, possibly skip over it.
-    if ('station_%s.zip' % n in os.listdir(datadir)) and skipexistingdata:
-        if verbose: print "Skipped station %s" % n
-        return None
-
-    # First access the weather station's data page.
-    page = urllib2.urlopen(
-        r'http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=136&p_display_type=dailyDataFile&p_startYear=&p_c=&p_stn_num='
-        + str(n).zfill(6)
-    )
-    soup = BeautifulSoup(page, 'html.parser')
-
-    # Then find the download link for that page.
-    download_link_extension = soup.find('a',
-        {'title': "Data file for daily rainfall data for all years"}
-    )['href']
-
-    # Open the download link (which is read as binary) and save it in the correct format (zip file).
-    data = urllib2.urlopen(str(r'http://www.bom.gov.au' + download_link_extension))
-    with open(datadir+'station_%s.zip' % n, 'wb') as zipper:
-        zipper.write(data.read())
-
-    # Finally, print a success message if verbose and return
-    if verbose: print "Download for station %s was successful!" % n
-    return None
-
-
-def unzip(zip_ref):
-    """Unzippes the reference specified into a new directory."""
-    basename = os.path.splitext(zip_ref)[0]
-    with zipfile.ZipFile(zip_ref, "r") as z:
-        z.extractall(basename)
-    return None
-
-
-def importRainfallData(csv_file):
-    "Given the rainfall csv file, it will import and clean the raw data."
-    df = pd.read_csv(csv_file)
-
-    # Rename the columns
-    df.rename(columns={
-        'Bureau of Meteorology station number': "Station Number",
-        'Rainfall amount (millimetres)': 'Rainfall',
-    }, inplace=True)
-
-    # Combine date into a single column.
-    df['Year'] = df['Year'].map(str)
-    df['Month'] = df['Month'].map(lambda x: str(x).zfill(2))
-    df['Day'] = df['Day'].map(lambda x: str(x).zfill(2))
-    df.insert(
-        2, 'Date',
-        df['Year'] + '-' +
-        df['Month'] + '-' +
-        df['Day']
-    )
-    df.drop(['Year', 'Month', 'Day'], axis=1, inplace=True)
-
-    # Next, drop the first and second columns.
-    df.drop(["Product code", "Station Number"], axis=1, inplace=True)
-
-    # Drop any rows that are before the start of data collection (i.e. drop Jan if started in Feb).
-    data_start = df['Rainfall'].first_valid_index()
-    data_finish = df['Rainfall'].last_valid_index()
-    df = df[data_start:data_finish]
-
-    # Set the date as the index column.
-    df.set_index('Date', inplace=True)
-
-    # Finally return the result.
-    return df
-
-
-def formatMultiIndexDataframe(dataframes_dict):
-    """By passing in the dictionary, a multi-indexed dataframe will be created."""
-    stations, dfs = zip(*dataframes_dict.items())
-    return pd.concat(dfs, keys=stations, names=['Station Number', 'Date'])
-
-
 def tidyUp(datadir='./'):
     """Goes through and cleans up after itself."""
     for f in glob.glob(datadir+'*.zip'):
         os.remove(f)
+    for f in glob.glob(datadir+'station*'):
+        shutil.rmtree(f)
     return
+
+def formatMultiIndexDataframe(dataframes_dict):
+    """By passing in a dictionary of dataframes, a multi-indexed dataframe will be created."""
+    stations, dfs = zip(*dataframes_dict.items())
+    return pd.concat(dfs, keys=stations, names=['Station Number', 'Date'])
+
+##################################
+# CREATE DATA PROCESSING CLASSES #
+##################################
+
+class WeatherStation(object):
+    """The parent class to all weather station's data collection classes."""
+    HOME = r'http://www.bom.gov.au'
+
+    def __init__(self, n):
+        """Initialise the station's number."""
+        # Initialise the variables.
+        self.n = n
+        self.webpage = ''  # NOTE: This must be defined in the children classes.
+        self.HTML_download_attribute = {}  # NOTE: This must be defined in the children classes.
+        self.datadir = 'Data/'
+        self.saveformat = 'station_%s' % self.n
+        return None
+
+    def downloadZippedData(self):
+        """Downloads the zipped data for the given station."""
+        # First get the download link.
+        page = urllib2.urlopen(self.webpage)
+        soup = BeautifulSoup(page, 'html.parser')
+        download_link_extension = soup.find(
+            'a', self.HTML_download_attribute
+        )['href']
+
+        # Then we can download it.
+        data = urllib2.urlopen(str(self.HOME + download_link_extension))
+        with open(self.datadir+self.saveformat+'.zip', 'wb') as f:
+            f.write(data.read())
+        return None
+
+    def unzipIt(self):
+        """Unzips the zipped file."""
+        zip_ref = self.datadir+self.saveformat+'.zip'
+        basename = os.path.splitext(zip_ref)[0]
+        with zipfile.ZipFile(zip_ref, "r") as z:
+            z.extractall(basename)
+        return None
+
+
+class RainfallWeatherStation(WeatherStation):
+    """The class that handles fetching the data for the rainfall."""
+
+    def __init__(self, n, autorun=True):
+        """Initialise the class."""
+        super(RainfallWeatherStation, self).__init__(n)
+        self.webpage = (
+            r'http://www.bom.gov.au/jsp/ncc/cdio/weatherData/av?p_nccObsCode=136&p_display_type=dailyDataFile&p_startYear=&p_c=&p_stn_num='
+            + str(self.n).zfill(6)
+        )
+        self.HTML_download_attribute = {'title': "Data file for daily rainfall data for all years"}
+
+        # Then call the functions in order to initialise everything else.
+        if autorun:
+            self.downloadZippedData()
+            self.unzipIt()
+            self.importIt()
+        return None
+
+    def importIt(self):
+        """Imports (and tidies) in the rainfall data."""
+        self.df = pd.read_csv(glob.glob(self.datadir+self.saveformat+'/*.csv')[0])
+
+        # Rename the columns
+        self.df.rename(columns={
+            'Bureau of Meteorology station number': "Station Number",
+            'Rainfall amount (millimetres)': 'Rainfall',
+        }, inplace=True)
+
+        # Combine date into a single column.
+        self.df['Year'] = self.df['Year'].map(str)
+        self.df['Month'] = self.df['Month'].map(lambda x: str(x).zfill(2))
+        self.df['Day'] = self.df['Day'].map(lambda x: str(x).zfill(2))
+        self.df.insert(
+            2, 'Date',
+            self.df['Year'] + '-' +
+            self.df['Month'] + '-' +
+            self.df['Day']
+        )
+        self.df.drop(['Year', 'Month', 'Day'], axis=1, inplace=True)
+
+        # Next, drop the first and second columns.
+        self.df.drop(["Product code", "Station Number"], axis=1, inplace=True)
+
+        # Drop any rows that are before the start of data collection (i.e. drop Jan if started in Feb).
+        data_start = self.df['Rainfall'].first_valid_index()
+        data_finish = self.df['Rainfall'].last_valid_index()
+        self.df = self.df[data_start:data_finish]
+
+        # Set the date as the index column.
+        self.df.set_index('Date', inplace=True)
+        return None
 
 
 def main():
     """Runs the main script."""
-    BOM_HOME = r'http://www.bom.gov.au'
     WEATHERSTATIONS_CSV = 'weather_stations.csv'
     DATA_DIRECTORY = 'Data/'
     EXPORT_NAME = 'weatherstations_WA.csv'
@@ -111,9 +133,8 @@ def main():
     dataframes = dict(); total_rows = len(station_list)
     for ii, n in station_list.iteritems():
         print "Station %s/%s" % (ii+1, total_rows)
-        downloadDataForStation(n, datadir=DATA_DIRECTORY, skipexistingdata=True)
-        unzip(DATA_DIRECTORY+'station_%s.zip' % n)
-        dataframes[n] = importRainfallData(glob.glob(DATA_DIRECTORY+'station_%s/*.csv' % n)[0])
+        station = RainfallWeatherStation(n)
+        dataframes[n] = station.df
     export_df = formatMultiIndexDataframe(dataframes)
     export_df.to_csv(EXPORT_NAME)
     tidyUp(datadir=DATA_DIRECTORY)
